0c37d74afc8617f339ae0fab9850ab6b60caf607
jonijaas <joni.jaaskelainen2@metropolia.fi>
2021-12-17 18:41:51 +0200

    puhelinluettelo axios tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 
 const Persons = ({ data }) => {
   return (
@@ -41,16 +42,18 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1231244" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [newFilter, setNewFilter] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const filterPerson = persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase()))
 
